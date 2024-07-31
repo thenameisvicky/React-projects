7d8402deca349a83bac5a86fe6bdf37ecf317f48
thenameisvicky <vigneshbalasubramaniyan3@gmail.com>
2024-07-31 21:11:43 +0530

    Added changes

--- News-mag/src/Components/Navbar.jsx
+++ News-mag/src/Components/Navbar.jsx
@@ -33,60 +33,60 @@ export const Navbar = ({ setCategory, setCountry }) => {
                                 Country
                             </a>
                             <ul className="dropdown-menu " style={{maxHeight:"200px", overflow:"auto"}}>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("in") }}>India</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("ae") }}>United Arab Emirates</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("ar") }}>Argentina</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("at") }}>Austria</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("au") }}>Australia</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("be") }}>Belgium</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("bg") }}>Bulgaria</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("br") }}>Brazil</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("ca") }}>Canada</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("ch") }}>Switzerland</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("cn") }}>China</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("us") }}>United States of America</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("co") }}>Colombia</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("cu") }}>Cuba</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("cz") }}>Czech Republic</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("de") }}>Germany</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("eg") }}>Egypt</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("fr") }}>France</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("gb") }}>United Kingdom</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("gr") }}>Greece</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("hk") }}>Hong Kong</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("hu") }}>Hungary</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("id") }}>Indonesia</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("ie") }}>Ireland</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("il") }}>Israel</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("it") }}>Italy</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("jp") }}>Japan</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("kr") }}>South Korea</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("lt") }}>Lithuania</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("lv") }}>Latvia</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("ma") }}>Morocco</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("mx") }}>Mexico</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("my") }}>Malaysia</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("ng") }}>Nigeria</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("nl") }}>Netherlands</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("no") }}>Norway</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("nz") }}>New Zealand</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("ph") }}>Philippines</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("pl") }}>Poland</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("pt") }}>Portugal</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("ro") }}>Romania</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("rs") }}>Serbia</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("ru") }}>Russia</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("sa") }}>Saudi Arabia</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("se") }}>Sweden</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("sg") }}>Singapore</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("si") }}>Slovenia</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("sk") }}>Slovakia</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("th") }}>Thailand</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("tr") }}>Turkey</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("tw") }}>Taiwan</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("ua") }}>Ukraine</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("ve") }}>Venezuela</a></li>
-                                <li><a className="dropdown-item" onClick={() => { setCountry("za") }}>South Africa</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("in") }}>India</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("ae") }}>United Arab Emirates</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("ar") }}>Argentina</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("at") }}>Austria</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("au") }}>Australia</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("be") }}>Belgium</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("bg") }}>Bulgaria</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("br") }}>Brazil</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("ca") }}>Canada</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("ch") }}>Switzerland</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("cn") }}>China</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("us") }}>United States of America</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("co") }}>Colombia</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("cu") }}>Cuba</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("cz") }}>Czech Republic</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("de") }}>Germany</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("eg") }}>Egypt</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("fr") }}>France</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("gb") }}>United Kingdom</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("gr") }}>Greece</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("hk") }}>Hong Kong</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("hu") }}>Hungary</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("id") }}>Indonesia</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("ie") }}>Ireland</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("il") }}>Israel</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("it") }}>Italy</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("jp") }}>Japan</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("kr") }}>South Korea</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("lt") }}>Lithuania</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("lv") }}>Latvia</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("ma") }}>Morocco</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("mx") }}>Mexico</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("my") }}>Malaysia</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("ng") }}>Nigeria</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("nl") }}>Netherlands</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("no") }}>Norway</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("nz") }}>New Zealand</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("ph") }}>Philippines</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("pl") }}>Poland</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("pt") }}>Portugal</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("ro") }}>Romania</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("rs") }}>Serbia</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("ru") }}>Russia</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("sa") }}>Saudi Arabia</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("se") }}>Sweden</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("sg") }}>Singapore</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("si") }}>Slovenia</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("sk") }}>Slovakia</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("th") }}>Thailand</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("tr") }}>Turkey</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("tw") }}>Taiwan</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("ua") }}>Ukraine</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("ve") }}>Venezuela</a></li>
+                                <li><a className="dropdown-item" style={{cursor:"pointer"}} onClick={() => { setCountry("za") }}>South Africa</a></li>
 
                             </ul>
                         </li>
